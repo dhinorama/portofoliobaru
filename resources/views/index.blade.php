968008b6fc9bb8d3ca7eafba5e91e91b5d39b2a7
dhinorama <dinoramadan2007@gmail.com>
2024-06-13 19:50:58 +0700

    huu

---
+++ resources/views/index.blade.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/sa.css">
+    <title>Nono Baru</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="header-left">
+                <a href="#"><img src="asset/img.jpg4578.png"></a>
+            </div>
+            <div class="header-right">
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#section1">Portofolio</a>
+                    </li>
+                    <li>
+                        <a href="#profile">Resume</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="intro" >
+        <div class="container">
+            <h2>HELLO</h2>
+            <h1>I'M NONO</h1>
+            <p>school in wikrama vocational high school</p>
+            <p>and I hope you like Fortfolio</p>
+            <div class="btn-intro">
+                <span>Let's start</span>
+            </div>
+            <img src="asset/introwork.png">
+        </div>
+    </div>
+    <div class="profile" id="profile">
+        <div class="container">
+            <div class="contents-profile">
+                <img class="pict-profile" src="asset/Gambar WhatsApp 2024-06-09 pukul 12.56.48_a2a15dc7.jpg">
+                <div class="info-cv">
+                    <h2>Hi, Nice to meet you.</h2>
+                    <p>My name is Dhino Ramadhan</p>
+                    <p>l was born in Bogor, West Java</p>
+                    <p>l'm currently focusing on deepening the html,css and javascript</p>
+                    <p>if you want to know more about me</p>
+                    <p>You can see in CV</p>
+                    <div class="btn-cv">
+                        <input type="button" value="Download CV">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main">
+        <div class="container">
+            <div class="main-wrapper">
+                <h1>MY SKILLS</h1>
+                <div class="skills">
+                    <div class="skill">
+                        <img src="asset/img.jpg369.png">
+                        <h2>HTML</h2>
+                        <p>The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.</p>
+                    </div>
+                    <div class="skill">
+                        <img src="asset/img.jpd145.png">
+                        <h2>CSS</h2>
+                        <p>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.</p>
+                    </div>
+                    <div class="skill">
+                        <img src="asset/img.jpd589.png">
+                        <h2>JAVASCRIPT</h2>
+                        <p>javaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.</p>
+                    </div>
+                </div>
+            </div>
+            <h1 class="section1" id="section1">WHAT I’VE BEEN UP TO</h1>
+            <p class="section2">Here’s a selection of some recent work</p>
+            <div class="project-result" > 
+                <div class="detail-mobile">
+                    <h1>End of year project</h1>
+                    <p>
+                        This is a project that I have worked on at the end of 2023,
+                        I use the PHP programming language,
+                        I also not only master the programming languages ​​HTML, CSS, and Java,
+                        The projects I made were student data, motorbike rental and fuel,
+                        and fuel and many others.
+                        but this is all I can show.
+                    </p>
+                    <div class="btn-mobile">
+                        <span>View Case Study</span>
+                    </div>
+                </div>
+                <div class="pict-mobile">
+                    <img src="asset/Gambar_WhatsApp_2024-06-09_pukul_14.48.49_0662a737-removebg-preview.png" >
+                </div>
+            </div>
+            <h1 class="section3">TESTIMONIALS</h1>
+            <div class="testimonials-people">
+                <img class="petik" src="asset/petik.png" >
+                <div class="testimonial">
+                    <p class="text-testimonial">
+                        They performed in all aspects excellently.
+                        They have given well-appreciated discounts at various stages of the project and also with suggestions that saved cost. 
+                        When it comes to delivering, they were always ahead of time. We don’t know how they do that but that’s very impressive. 
+                        Their agility and customer support is on a different level, well appreciated.
+                    </p>
+                    <div class="profile-testimonial">
+                        <img src="asset/Ellipse 20.png" >
+                        <h2>WILLIAM KEN</h2>
+                        <p>Director of Kotakode</p>
+                    </div>
+                </div>
+                <img class="petik" src="asset/petik.png" >
+                <div class="testimonial">
+                    <p class="text-testimonial">
+                        I hired Coderay to help build my fitness and food log mobile apps. 
+                        Professional, knowledgeable, and skilled, Space-O delivered a detailed mock app and a fully featured product that launched successfully. 
+                        The team communicated well and readily grasped concept and intent. Their communication and ability to understand requests were top-notch. 
+                        I was most impressed with their professionalism, their extensive end-to-end knowledge, and their design capabilities.
+                    </p>
+                    <div class="profile-testimonial">
+                        <img src="asset/Ellipse 21.png" >
+                        <h2>DEVINA VALEN</h2>
+                        <p>CEO Build With Arsen</p>
+                    </div>  
+                </div>
+            </div>
+            <h1 class="section3">GET IN TOUCH</h1>
+            <p class="section4">Interested in working together?</p>
+            <p class="section4">Feel free to contact me for any project or colaboration</p>
+            <div class="contact" id="contact">
+                <div class="gif-img">
+                    <img src="asset/gifwork.gif">
+                </div>
+                <div class=inputs>
+                    <div class="input-first">
+                        <input type="text" placeholder="Name">
+                        <input type="text" placeholder="Email">
+                    </div>
+                    <input class="subject-form" type="text" placeholder="Subject">
+                    <textarea placeholder="Message" ></textarea>
+                    <div class="btn-send">
+                        <span>Send</span>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container">
+            <div class="socmed" >
+                <a href="">
+                    <img src="asset/linkedin.png" >
+                </a>
+                <a href="https://www.instagram.com/drmrlee/">
+                    <img src="asset/ig.png" >
+                </a>
+                <a href="https://github.com/dhinorama">
+                    <img src="asset/github.png" >
+                </a>
+                <a href="https://youtu.be/ThI0pBAbFnk?si=yJhNH9JvUajftE0E">
+                    <img src="asset/yt.png" >
+                </a>
+            </div>
+            <p>©2023 All Right Reserved - By Dhino Ramadhan</p>
+        </div>
+    </footer>
+</body>
+</html>
